a99112d9aba961e951333842c9a7e80796ea137c
home <silvia.homam@gmail.com>
2022-08-21 03:27:07 +0300

    small

--- internal/models/entity.go
+++ internal/models/entity.go
@@ -1,2 +1,18 @@
 package entity
 
+import (
+	"time"
+)
+
+type Entity struct {
+	Payload string     `json:"payload"`
+	TTL *time.Duration `json:"ttl"`
+}
+
+func (e Entity) GetTTL() *time.Duration {
+	return e.TTL
+}
+
+
+
+
